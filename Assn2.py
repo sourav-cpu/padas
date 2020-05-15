71f0c5533b61ef0d2b02557ab380145360ee7c5d
sourav-cpu <64347351+sourav-cpu@users.noreply.github.com>
2020-05-15 10:48:19 +0530

    Create Assn2.py

---
+++ Assn2.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+%matplotlib inline
+import numpy as np
+df=pd.read_csv("student-math.csv")
+df['final grade']=df['G1'] + df['G2'] + df['G3']
+df.drop(['G1'],['G2'],['G3'],axis=1,inplace=True)
+df.replace({
+    'Yes':1,
+    'No':0
+})
+area=np.pi*3
+plt.scatter(df["studytime"],df["finalgrade"],c=green,s=area,alpha=0.5
+plt.xlabel("studytime")
+plt.ylabel("finalgrade")
+plt.show()
